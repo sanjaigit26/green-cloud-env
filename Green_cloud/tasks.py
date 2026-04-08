ce0efc692368347502e5ce7321b4aeaa2b5a4a3a
sanjaigit26 <sanjaigit25@gmail.com>
2026-04-08 21:20:35 +0530

    Update tasks.py

--- Green_cloud/tasks.py
+++ Green_cloud/tasks.py
@@ -1,11 +1,16 @@
+from grader import grade_easy, grade_medium, grade_hard
+
 TASKS = {
     "easy": {
         "description": "Assign all jobs to any region",
+        "grader": grade_easy,
     },
     "medium": {
         "description": "Assign all jobs before their deadlines",
+        "grader": grade_medium,
     },
     "hard": {
         "description": "Minimize carbon emissions while meeting deadlines",
+        "grader": grade_hard,
     }
 }
